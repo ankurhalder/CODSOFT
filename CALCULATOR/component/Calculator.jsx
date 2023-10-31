1d449eb58d3a2e72fc966ec69f7d8c729b9cae13
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-10-31 16:27:14 +0530

    update 4

--- CALCULATOR/component/Calculator.jsx
+++ CALCULATOR/component/Calculator.jsx
@@ -15,10 +15,11 @@ class Calculator extends Component {
 		if (value === "=") {
 			try {
 				const result = eval(this.state.display);
+				const historyEntry = `${this.state.display} = ${result}`;
 				this.setState((prevState) => ({
 					display: result,
 					isResult: true,
-					history: [...prevState.history, prevState.display + "=" + result],
+					history: [historyEntry, ...prevState.history],
 				}));
 			} catch (error) {
 				this.setState({ display: "Error", isResult: true });
@@ -34,9 +35,10 @@ class Calculator extends Component {
 		} else if (value === "Reset") {
 			this.setState({ display: "", isResult: false });
 		} else if (value === "History") {
-			// Show calculation history in the display
-			const historyString = this.state.history.join("\n");
-			this.setState({ display: historyString, isResult: true });
+			// Toggle history display
+			this.setState((prevState) => ({
+				isResult: !prevState.isResult,
+			}));
 		} else {
 			this.setState({ display: this.state.display + value, isResult: false });
 		}
@@ -51,153 +53,165 @@ class Calculator extends Component {
 					value={this.state.display}
 					readOnly
 				/>
-				<div className="buttons">
-					<button
-						onClick={() => this.handleClick("7")}
-						className="number-button"
-					>
-						7
-					</button>
-					<button
-						onClick={() => this.handleClick("8")}
-						className="number-button"
-					>
-						8
-					</button>
-					<button
-						onClick={() => this.handleClick("9")}
-						className="number-button"
-					>
-						9
-					</button>
-					<button
-						onClick={() => this.handleClick("+")}
-						className="operator-button"
-					>
-						+
-					</button>
-					<button
-						onClick={() => this.handleClick("4")}
-						className="number-button"
-					>
-						4
-					</button>
-					<button
-						onClick={() => this.handleClick("5")}
-						className="number-button"
-					>
-						5
-					</button>
-					<button
-						onClick={() => this.handleClick("6")}
-						className="number-button"
-					>
-						6
-					</button>
-					<button
-						onClick={() => this.handleClick("-")}
-						className="operator-button"
-					>
-						-
-					</button>
-					<button
-						onClick={() => this.handleClick("1")}
-						className="number-button"
-					>
-						1
-					</button>
-					<button
-						onClick={() => this.handleClick("2")}
-						className="number-button"
-					>
-						2
-					</button>
-					<button
-						onClick={() => this.handleClick("3")}
-						className="number-button"
-					>
-						3
-					</button>
-					<button
-						onClick={() => this.handleClick("*")}
-						className="operator-button"
-					>
-						*
-					</button>
-					<button
-						onClick={() => this.handleClick("0")}
-						className="number-button"
-					>
-						0
-					</button>
-					<button
-						onClick={() => this.handleClick(".")}
-						className="number-button"
-					>
-						.
-					</button>
-					<button
-						onClick={() => this.handleClick("CE")}
-						className="function-button"
-					>
-						CE
-					</button>
-					<button
-						onClick={() => this.handleClick("=")}
-						className="equals-button"
-					>
-						=
-					</button>
-					<button
-						onClick={() => this.handleClick("/")}
-						className="operator-button"
-					>
-						/
-					</button>
-					<button
-						onClick={() => this.handleClick("(")}
-						className="number-button"
-					>
-						(
-					</button>
-					<button
-						onClick={() => this.handleClick(")")}
-						className="number-button"
-					>
-						)
-					</button>
-					<button
-						onClick={() => this.handleClick("x^2")}
-						className="function-button"
-					>
-						x^2
-					</button>
-					<button
-						onClick={() => this.handleClick("Math.PI")}
-						className="number-button"
-					>
-						π
-					</button>
-					<button
-						onClick={() => this.handleClick("n!")}
-						className="function-button"
-					>
-						n!
-					</button>
-					<button
-						onClick={() => this.handleClick("Reset")}
-						className="function-button"
-					>
-						Reset
-					</button>
-					<button
-						onClick={() => this.handleClick("History")}
-						className="function-button"
-					>
-						History
-					</button>
-					{/* Add more buttons with appropriate classNames for other functionalities as needed */}
-				</div>
+				{this.state.isResult ? (
+					<div className="history-display">
+						<div className="history-tape">
+							{this.state.history.map((entry, index) => (
+								<div key={index} className="history-entry">
+									{entry}
+								</div>
+							))}
+						</div>
+					</div>
+				) : (
+					<div className="buttons">
+						<button
+							onClick={() => this.handleClick("7")}
+							className="number-button"
+						>
+							7
+						</button>
+						<button
+							onClick={() => this.handleClick("8")}
+							className="number-button"
+						>
+							8
+						</button>
+						<button
+							onClick={() => this.handleClick("9")}
+							className="number-button"
+						>
+							9
+						</button>
+						<button
+							onClick={() => this.handleClick("+")}
+							className="operator-button"
+						>
+							+
+						</button>
+						<button
+							onClick={() => this.handleClick("4")}
+							className="number-button"
+						>
+							4
+						</button>
+						<button
+							onClick={() => this.handleClick("5")}
+							className="number-button"
+						>
+							5
+						</button>
+						<button
+							onClick={() => this.handleClick("6")}
+							className="number-button"
+						>
+							6
+						</button>
+						<button
+							onClick={() => this.handleClick("-")}
+							className="operator-button"
+						>
+							-
+						</button>
+						<button
+							onClick={() => this.handleClick("1")}
+							className="number-button"
+						>
+							1
+						</button>
+						<button
+							onClick={() => this.handleClick("2")}
+							className="number-button"
+						>
+							2
+						</button>
+						<button
+							onClick={() => this.handleClick("3")}
+							className="number-button"
+						>
+							3
+						</button>
+						<button
+							onClick={() => this.handleClick("*")}
+							className="operator-button"
+						>
+							*
+						</button>
+						<button
+							onClick={() => this.handleClick("0")}
+							className="number-button"
+						>
+							0
+						</button>
+						<button
+							onClick={() => this.handleClick(".")}
+							className="number-button"
+						>
+							.
+						</button>
+						<button
+							onClick={() => this.handleClick("CE")}
+							className="function-button"
+						>
+							CE
+						</button>
+						<button
+							onClick={() => this.handleClick("=")}
+							className="equals-button"
+						>
+							=
+						</button>
+						<button
+							onClick={() => this.handleClick("/")}
+							className="operator-button"
+						>
+							/
+						</button>
+						<button
+							onClick={() => this.handleClick("(")}
+							className="number-button"
+						>
+							(
+						</button>
+						<button
+							onClick={() => this.handleClick(")")}
+							className="number-button"
+						>
+							)
+						</button>
+						<button
+							onClick={() => this.handleClick("x^2")}
+							className="function-button"
+						>
+							x^2
+						</button>
+						<button
+							onClick={() => this.handleClick("Math.PI")}
+							className="number-button"
+						>
+							π
+						</button>
+						<button
+							onClick={() => this.handleClick("n!")}
+							className="function-button"
+						>
+							n!
+						</button>
+						<button
+							onClick={() => this.handleClick("Reset")}
+							className="function-button"
+						>
+							Reset
+						</button>
+						<button
+							onClick={() => this.handleClick("History")}
+							className="function-button"
+						>
+							History
+						</button>
+						{/* Add more buttons with appropriate classNames for other functionalities as needed */}
+					</div>
+				)}
 			</div>
 		);
 	}
