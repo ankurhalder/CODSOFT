1085091c878cea18a0b1d9d5f1ca0ca79febcdb0
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-01 16:03:42 +0530

    Logo fixation

--- TODOLIST/CLIENT/containers/Header/Navbar.jsx
+++ TODOLIST/CLIENT/containers/Header/Navbar.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Navbar() {
 	return (
 		<nav className="navbar">
-			<div className="navbar__logo"></div>
+			<div className="navbar__logo">
+				<img src="/mytoodle.png" alt="mytoodle" />
+				<label htmlFor="mytoddle">MyToodle</label>
+			</div>
 			<div className="navbar__buttons">
 				<button className="navbar__login">Login</button>
 				<button className="navbar__signup">Sign Up</button>
