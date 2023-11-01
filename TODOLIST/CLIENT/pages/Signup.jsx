28ed37761c0442c84a98578c90b28563e73c9d8f
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 00:44:16 +0530

    SignUp form added

--- TODOLIST/CLIENT/pages/Signup.jsx
+++ TODOLIST/CLIENT/pages/Signup.jsx
@@ -1,7 +1,95 @@
+import React, { useState } from "react";
+
 function SignUp() {
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// You can add your signup logic here, like sending data to the server.
+		// Remember to validate the form fields and handle errors accordingly.
+		// For simplicity, we won't implement the actual signup logic in this example.
+	};
+
 	return (
-		<div>
-			<h1>SignUp</h1>
+		<div className="signup-container">
+			<h2>Sign Up</h2>
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label htmlFor="firstName">First Name</label>
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						value={formData.firstName}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="lastName">Last Name</label>
+					<input
+						type="text"
+						id="lastName"
+						name="lastName"
+						value={formData.lastName}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						value={formData.email}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						value={formData.password}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="confirmPassword">Confirm Password</label>
+					<input
+						type="password"
+						id="confirmPassword"
+						name="confirmPassword"
+						value={formData.confirmPassword}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<button type="submit">Sign Up</button>
+			</form>
 		</div>
 	);
 }
