ea477bbf5877553dfa383b1388a6d15622d8850e
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-01 15:55:06 +0530

    navbar added

---
+++ TODOLIST/CLIENT/containers/Header/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<nav className="navbar">
+			<div className="navbar__logo">Your Logo</div>
+			<div className="navbar__buttons">
+				<button className="navbar__login">Login</button>
+				<button className="navbar__signup">Sign Up</button>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
