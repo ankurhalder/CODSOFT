bb8ec9fefe2760c44abec52ce8c0f1d2a31c3e62
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 00:52:52 +0530

    Modified signUp jsx

--- TODOLIST/CLIENT/pages/Signup.jsx
+++ TODOLIST/CLIENT/pages/Signup.jsx
@@ -9,6 +9,9 @@ function SignUp() {
 		confirmPassword: "",
 	});
 
+	const [passwordError, setPasswordError] = useState("");
+	const [emailError, setEmailError] = useState("");
+
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setFormData({
@@ -16,12 +19,41 @@ function SignUp() {
 			[name]: value,
 		});
 	};
+	const validatePassword = (password) => {
+		const passwordRegex =
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+		return passwordRegex.test(password);
+	};
+
+	const validateEmail = (email) => {
+		const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+		return emailRegex.test(email);
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		// You can add your signup logic here, like sending data to the server.
-		// Remember to validate the form fields and handle errors accordingly.
-		// For simplicity, we won't implement the actual signup logic in this example.
+
+		if (formData.password !== formData.confirmPassword) {
+			setPasswordError("Passwords do not match");
+			return;
+		} else if (!validatePassword(formData.password)) {
+			setPasswordError(
+				"Password must have at least 8 characters, 1 uppercase, 1 lowercase, 1 special character, and 1 number"
+			);
+			return;
+		} else {
+			setPasswordError("");
+		}
+
+		// Email validation
+		if (!validateEmail(formData.email)) {
+			setEmailError("Invalid email format");
+			return;
+		} else {
+			setEmailError("");
+		}
+
+		// You can proceed with your signup logic here
 	};
 
 	return (
@@ -87,6 +119,10 @@ function SignUp() {
 						required
 					/>
 				</div>
+
+				{passwordError && <p className="error-message">{passwordError}</p>}
+
+				{emailError && <p className="error-message">{emailError}</p>}
 
 				<button type="submit">Sign Up</button>
 			</form>
