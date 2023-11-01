0bd2f5fc0b12361a575793fb68660175353d5fa2
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-01 16:24:39 +0530

    Router implemented successfully

--- TODOLIST/CLIENT/src/App.jsx
+++ TODOLIST/CLIENT/src/App.jsx
@@ -1,13 +1,18 @@
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../layout/Layout";
 import { SignIn, SignUp, NoPage } from "../pages";
 function App() {
 	return (
 		<Fragment>
 			<BrowserRouter>
-				<Routes>
-					<Route></Route>
-				</Routes>
+				<Layout>
+					<Routes>
+						<Route path="/" element={<SignIn />} />
+						<Route path="/signup" element={<SignUp />} />
+						<Route path="*" element={<NoPage />} />
+					</Routes>
+				</Layout>
 			</BrowserRouter>
 		</Fragment>
 	);
