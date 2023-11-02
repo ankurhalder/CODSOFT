e994e6e12be26a45e854efc069da0ca676085143
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 18:07:29 +0530

    dual ball loader added

---
+++ TODOLIST/CLIENT/components/loading/DualBallLoader.jsx
@@ -0,0 +1,13 @@
+const DualBallLoader = () => {
+	return (
+		<div className="custom-dual-ball-loader">
+			<div className="ball-container">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default DualBallLoader;
