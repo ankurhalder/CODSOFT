bca7ac9e21bd21d64e617155fb63d1f0d175c1cb
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 18:16:28 +0530

    Wedges Loader added

---
+++ TODOLIST/CLIENT/components/loading/WedgesLoader.jsx
@@ -0,0 +1,30 @@
+const WedgesLoader = () => {
+	return (
+		<div className="custom-wedges-loader">
+			<div className="wedges-container">
+				<div>
+					<div>
+						<div></div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div></div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div></div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default WedgesLoader;
