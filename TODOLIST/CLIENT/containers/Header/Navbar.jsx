cad65b7305141017399b32da1c76b5c89c4410fb
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 20:03:22 +0530

    updated navbar

--- TODOLIST/CLIENT/containers/Header/Navbar.jsx
+++ TODOLIST/CLIENT/containers/Header/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 function Navbar() {
+	const authToken = localStorage.getItem("authToken");
+
 	return (
 		<nav className="navbar">
 			<div className="navbar__logo">
@@ -8,24 +11,33 @@ function Navbar() {
 				<label htmlFor="mytoddle">MyToodle</label>
 			</div>
 			<div className="navbar__menu">
-				<ul className="menu__items">
-					<li className="item">
-						<Link to="/">Sample</Link>
-					</li>
-					<li className="item">
-						<Link to="/signup">Sample</Link>
-					</li>
-				</ul>
+				{authToken && (
+					<ul className="menu__items">
+						<li className="item">
+							<Link to="/">Sample</Link>
+						</li>
+						<li className="item">
+							<Link to="/dashboard">Dashboard</Link>
+						</li>
+					</ul>
+				)}
 			</div>
 			<div className="navbar__buttons">
-				<Link to="/">
-					<button className="navbar__signin">Sign In</button>
-				</Link>
-				<Link to="signup">
-					<button className="navbar__signup">Sign Up</button>
-				</Link>
+				{authToken ? (
+					<button className="navbar__signout">Sign Out</button>
+				) : (
+					<>
+						<Link to="/signin">
+							<button className="navbar__signin">Sign In</button>
+						</Link>
+						<Link to="/signup">
+							<button className="navbar__signup">Sign Up</button>
+						</Link>
+					</>
+				)}
 			</div>
 		</nav>
 	);
 }
+
 export default Navbar;
