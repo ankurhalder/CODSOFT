812fae93604939827d418a10e74ca40e10185e95
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 17:32:42 +0530

    a loading spinner added

---
+++ TODOLIST/CLIENT/components/loading/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+const LoadingSpinner = () => {
+	return (
+		<div className="custom-loading-spinner">
+			<div className="spinner-container">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default LoadingSpinner;
