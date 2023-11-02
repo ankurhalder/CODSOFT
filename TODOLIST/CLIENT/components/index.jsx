fc8c51fb41178336da2780493b5d541d0d83f394
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 17:54:20 +0530

    Chunk Loader loading component added

--- TODOLIST/CLIENT/components/index.jsx
+++ TODOLIST/CLIENT/components/index.jsx
@@ -1,1 +1,2 @@
 export { default as LoadingSpinner } from "./loading/LoadingSpinner.jsx";
+export { default as ChunkLoader } from "./loading/ChunkLoader.jsx";
---
+++ TODOLIST/CLIENT/components/loading/ChunkLoader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ChunkLoader.scss";
+
+const ChunkLoader = () => {
+	return (
+		<div className="custom-chunk-loader">
+			<div className="loader-container">
+				<div>
+					<div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ChunkLoader;
