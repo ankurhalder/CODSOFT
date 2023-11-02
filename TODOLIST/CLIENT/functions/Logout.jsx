d7ad2fb3bb3dcf622eed2124804a3c7411b697ef
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-02 19:48:02 +0530

    Logout function added

---
+++ TODOLIST/CLIENT/functions/Logout.jsx
@@ -0,0 +1,42 @@
+const logoutUser = async () => {
+	try {
+		const authToken = localStorage.getItem("authToken");
+
+		if (authToken) {
+			const apiUrl = "http://localhost:8000/api/v1/users/logout";
+			const requestOptions = {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${authToken}`,
+				},
+			};
+
+			const response = await fetch(apiUrl, requestOptions);
+
+			if (!response.ok) {
+				throw new Error("Failed to log out. Please try again.");
+			}
+
+			const data = await response.json();
+
+			console.log("Logout successful:", data);
+
+			localStorage.removeItem("authToken");
+			localStorage.removeItem("email");
+			localStorage.removeItem("firstName");
+			localStorage.removeItem("lastName");
+			localStorage.removeItem("role");
+
+			window.location.href = "/";
+			return data;
+		} else {
+			console.error("No authentication token found. User is not logged in.");
+		}
+	} catch (error) {
+		console.error("Error logging out:", error);
+		throw error;
+	}
+};
+
+export default logoutUser;
