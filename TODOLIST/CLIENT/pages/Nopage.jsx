b5ffdc4894f0ebcfd67d0f6bf4e3aad5838bf46e
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-03 22:39:59 +0530

    page not found Page is ready

--- TODOLIST/CLIENT/pages/Nopage.jsx
+++ TODOLIST/CLIENT/pages/Nopage.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import { LandingLayout } from "../layout";
+// import { LandingLayout } from "../layout";
 function NoPage() {
 	return (
 		<Fragment>
-			<LandingLayout>
-				<h1>404</h1>
-			</LandingLayout>
+			{/* <LandingLayout> */}
+			<h1 className="nopage">404</h1>
+			<h2 className="page-not-found">Page Not Found</h2>
+			{/* </LandingLayout> */}
 		</Fragment>
 	);
 }
