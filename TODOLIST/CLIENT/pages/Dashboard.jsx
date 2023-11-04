9ec364df2ffd9d71d87a70c3421dc078b3579633
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-04 16:49:15 +0530

    dashboard making is on the way -1

--- TODOLIST/CLIENT/pages/Dashboard.jsx
+++ TODOLIST/CLIENT/pages/Dashboard.jsx
@@ -7,9 +7,22 @@ function Dashboard() {
 			window.location.href = "/";
 		}
 	}, []);
+	// console.log("authToken:", localStorage.getItem("authToken"));
+	const email = localStorage.getItem("email");
+	const firstName = localStorage.getItem("firstName");
+	const lastName = localStorage.getItem("lastName");
+	const role = localStorage.getItem("role");
+	console.log("email:", email);
+	console.log("firstName:", firstName);
+	console.log("lastName:", lastName);
+	console.log("role:", role);
 	return (
 		<Fragment>
-			<UserLayout></UserLayout>
+			<UserLayout>
+				<div>
+					<h1>welcome </h1>
+				</div>
+			</UserLayout>
 		</Fragment>
 	);
 }
