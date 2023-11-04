03850c12d156b8d90719127f5fb6733c719b0636
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-04 18:00:50 +0530

    authController defined

--- TODOLIST/SERVER/routes/todoRoutes.js
+++ TODOLIST/SERVER/routes/todoRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const todoController = require("../controller/todoController");
-
+const authController = require("../controller/authController");
 const router = express.Router();
 
 router
