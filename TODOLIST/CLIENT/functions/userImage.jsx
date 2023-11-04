d4b88b219f8963c64cbd0f7d1e6bec2c839b5301
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-05 00:31:29 +0530

    user imahe handled

---
+++ TODOLIST/CLIENT/functions/userImage.jsx
@@ -0,0 +1,84 @@
+const updateUserImage = async (file) => {
+	try {
+		console.log("updateUserImage function called");
+		const authToken = localStorage.getItem("token");
+
+		if (!authToken) {
+			throw new Error("Authentication token is missing.");
+		}
+
+		const formData = new FormData();
+		formData.append("profileImage", file);
+
+		const response = await fetch(
+			"http://localhost:8000/api/v1/profileImage/uploadProfileImage",
+			{
+				method: "PATCH",
+				headers: {
+					Authorization: `Bearer ${authToken}`,
+				},
+				body: formData,
+			}
+		);
+
+		if (!response.ok) {
+			const errorData = await response.json();
+			console.error(errorData);
+			throw new Error(errorData.message || "Image upload failed.");
+		}
+
+		const data = await response.json();
+		console.log(data);
+
+		if (data.status === "success") {
+			return data.data.user.profileImage;
+		} else {
+			throw new Error("Image upload was not successful.");
+		}
+	} catch (error) {
+		console.error("Image upload error:", error);
+		throw error;
+	}
+};
+
+const getUserProfileImage = async () => {
+	try {
+		const authToken = localStorage.getItem("token");
+
+		if (!authToken) {
+			throw new Error("Authentication token is missing.");
+		}
+
+		const response = await fetch(
+			"https://mylore.onrender.com/api/v1/profileImage/getUserProfileImage",
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${authToken}`,
+				},
+			}
+		);
+
+		if (!response.ok) {
+			const errorData = await response.json();
+			console.error(errorData);
+			throw new Error(
+				errorData.message || "Failed to fetch user profile image."
+			);
+		}
+
+		const blob = await response.blob();
+
+		if (blob.size > 0) {
+			const imageUrl = URL.createObjectURL(blob);
+			return imageUrl;
+		} else {
+			throw new Error("No image data received.");
+		}
+	} catch (error) {
+		console.error("Error fetching user profile image:", error);
+		throw error;
+	}
+};
+
+export { updateUserImage, getUserProfileImage };
