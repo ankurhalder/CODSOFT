261f4caebd73775452b57f16fe009cf674b677f3
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-05 01:03:24 +0530

    cors policy

--- TODOLIST/SERVER/app.js
+++ TODOLIST/SERVER/app.js
@@ -13,7 +13,7 @@ app.use(
 	cors({
 		origin: [
 			"http://localhost:3000",
-			"https://mylore.netlify.app",
+			"https://mytoodle.netlify.app/",
 			"http://localhost:3001",
 			"http://localhost:5173",
 		],
