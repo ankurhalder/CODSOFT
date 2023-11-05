9d8ae9e9afd4ac3cfac91dbf4c956f86418b70eb
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-05 17:03:04 +0530

    fix-17

--- TODOLIST/CLIENT/pages/Dashboard.jsx
+++ TODOLIST/CLIENT/pages/Dashboard.jsx
@@ -9,7 +9,14 @@ import {
 import { updateUserImage, getUserProfileImage } from "../functions/userImage";
 
 function formatDate(dateStr) {
-	const options = { year: "numeric", month: "long", day: "numeric" };
+	const options = {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+	};
 	return new Date(dateStr).toLocaleDateString(undefined, options);
 }
 
