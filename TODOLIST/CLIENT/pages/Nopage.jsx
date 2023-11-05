61257f191dffdbbde4ba952a6644814d8b3fa86d
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-05 14:46:03 +0530

    minor error

--- TODOLIST/CLIENT/pages/Nopage.jsx
+++ TODOLIST/CLIENT/pages/Nopage.jsx
@@ -1,12 +1,9 @@
 import { Fragment } from "react";
-// import { LandingLayout } from "../layout";
 function NoPage() {
 	return (
 		<Fragment>
-			{/* <LandingLayout> */}
 			<h1 className="nopage">404</h1>
 			<h2 className="page-not-found">Page Not Found</h2>
-			{/* </LandingLayout> */}
 		</Fragment>
 	);
 }
