449315b1c9079fa8042adc1c4636f10de732a383
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-06 23:19:55 +0530

    no page added

---
+++ PORTFOLIO/page/Nopage.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+function NoPage() {
+	return (
+		<Fragment>
+			<h1 className="nopage">404</h1>
+			<h2 className="page-not-found">Page Not Found</h2>
+		</Fragment>
+	);
+}
+
+export default NoPage;
--- PORTFOLIO/page/index.jsx
+++ PORTFOLIO/page/index.jsx
@@ -3,3 +3,4 @@ export { default as ContactMe } from "./ContactMe";
 export { default as MyProjects } from "./MyProjects";
 export { default as MyResume } from "./MyResume";
 export { default as MySkills } from "./MySkills";
+export { default as NoPage } from "./Nopage";
