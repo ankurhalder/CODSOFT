06c0afad74e621850f82d67d37a1202aa38f0540
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-06 23:31:37 +0530

    Router implementing

--- PORTFOLIO/src/App.jsx
+++ PORTFOLIO/src/App.jsx
@@ -1,8 +1,17 @@
+import { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
 	return (
-		<div>
-			<h1>Ankur Halder</h1>
-		</div>
+		<Fragment>
+			<BrowserRouter>
+				<Routes>
+					<div>
+						<h1>Ankur Halder</h1>
+					</div>
+				</Routes>
+			</BrowserRouter>
+		</Fragment>
 	);
 }
 
