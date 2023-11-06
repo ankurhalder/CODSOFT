3c09435c41f5d41b6e20fe03d9efbe71252b6a17
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-06 23:17:24 +0530

    all page exported

---
+++ PORTFOLIO/page/index.jsx
@@ -0,0 +1,5 @@
+export { default as AboutMe } from "./AboutMe";
+export { default as ContactMe } from "./ContactMe";
+export { default as MyProjects } from "./MyProjects";
+export { default as MyResume } from "./MyResume";
+export { default as MySkills } from "./MySkills";
