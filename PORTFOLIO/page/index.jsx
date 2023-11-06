7fcb2e4c8432ca7f7ff7099bd75d28dcbf1eae52
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-06 23:35:57 +0530

    Router implemented

--- PORTFOLIO/page/index.jsx
+++ PORTFOLIO/page/index.jsx
@@ -4,3 +4,4 @@ export { default as MyProjects } from "./MyProjects";
 export { default as MyResume } from "./MyResume";
 export { default as MySkills } from "./MySkills";
 export { default as NoPage } from "./Nopage";
+export { default as DashBoard } from "./DashBoard";
--- PORTFOLIO/src/App.jsx
+++ PORTFOLIO/src/App.jsx
@@ -1,14 +1,22 @@
+/* eslint-disable no-unused-vars */
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import {
+	DashBoard,
+	AboutMe,
+	ContactMe,
+	MyProjects,
+	MyResume,
+	MySkills,
+	NoPage,
+} from "../page";
 function App() {
 	return (
 		<Fragment>
 			<BrowserRouter>
 				<Routes>
-					<div>
-						<h1>Ankur Halder</h1>
-					</div>
+					<Route path="/" element={DashBoard} />
+					<Route path="*" element={NoPage} />
 				</Routes>
 			</BrowserRouter>
 		</Fragment>
