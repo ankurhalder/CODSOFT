e7bb220fb57cbb23b24f0f31519fef5f4ec2d732
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-08 02:13:41 +0530

    minor modifications

--- PORTFOLIO/containers/header/Navbar.jsx
+++ PORTFOLIO/containers/header/Navbar.jsx
@@ -11,48 +11,50 @@ function Navbar({ isMenuOpen, setMenuOpen }) {
 	};
 
 	return (
-		<nav className={`navbar ${isMenuOpen ? "open" : ""}`}>
-			<div className="menu-toggle" onClick={toggleMenu}>
-				<div className="bar"></div>
-				<div className="bar"></div>
-				<div className="bar"></div>
-			</div>
-			<div className="logo">
-				<img src="/Ankur_Halder.jpg" alt="Ankur Halder" />
-			</div>
-			<ul className="menu">
-				<li>
-					<Link to="/" onClick={closeMenu}>
-						Home
-					</Link>
-				</li>
-				<li>
-					<Link to="/about" onClick={closeMenu}>
-						About
-					</Link>
-				</li>
-				<li>
-					<Link to="/contact" onClick={closeMenu}>
-						Contact
-					</Link>
-				</li>
-				<li>
-					<Link to="/projects" onClick={closeMenu}>
-						Projects
-					</Link>
-				</li>
-				<li>
-					<Link to="/resume" onClick={closeMenu}>
-						Resume
-					</Link>
-				</li>
-				<li>
-					<Link to="/skills" onClick={closeMenu}>
-						Skills
-					</Link>
-				</li>
-			</ul>
-		</nav>
+		<header>
+			<nav className={`navbar ${isMenuOpen ? "open" : ""}`}>
+				<div className="menu-toggle" onClick={toggleMenu}>
+					<div className="bar"></div>
+					<div className="bar"></div>
+					<div className="bar"></div>
+				</div>
+				<div className="logo">
+					<img src="/Ankur_Halder.jpg" alt="Ankur Halder" />
+				</div>
+				<ul className="menu">
+					<li>
+						<Link to="/" onClick={closeMenu}>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to="/about" onClick={closeMenu}>
+							About
+						</Link>
+					</li>
+					<li>
+						<Link to="/contact" onClick={closeMenu}>
+							Contact
+						</Link>
+					</li>
+					<li>
+						<Link to="/projects" onClick={closeMenu}>
+							Projects
+						</Link>
+					</li>
+					<li>
+						<Link to="/resume" onClick={closeMenu}>
+							Resume
+						</Link>
+					</li>
+					<li>
+						<Link to="/skills" onClick={closeMenu}>
+							Skills
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 }
 
