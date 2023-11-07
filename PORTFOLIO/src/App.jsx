55a5714efca1bdbdbb4e03a2ef78fede2343c965
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-07 22:04:03 +0530

    router extended

--- PORTFOLIO/src/App.jsx
+++ PORTFOLIO/src/App.jsx
@@ -17,6 +17,11 @@ function App() {
 				<Routes>
 					<Route path="/" element={<DashBoard></DashBoard>} />
 					<Route path="*" element={<NoPage></NoPage>} />
+					<Route path="/about" element={<AboutMe></AboutMe>} />
+					<Route path="/contact" element={<ContactMe></ContactMe>} />
+					<Route path="/projects" element={<MyProjects></MyProjects>} />
+					<Route path="/resume" element={<MyResume></MyResume>} />
+					<Route path="/skills" element={<MySkills></MySkills>} />
 				</Routes>
 			</BrowserRouter>
 		</Fragment>
