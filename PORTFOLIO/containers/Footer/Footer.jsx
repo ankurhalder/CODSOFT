1e7180e723d470f976ad92e6ae289877ec2c4439
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-08 02:52:21 +0530

    updated Footer

--- PORTFOLIO/containers/Footer/Footer.jsx
+++ PORTFOLIO/containers/Footer/Footer.jsx
@@ -4,72 +4,73 @@ import { Link } from "react-router-dom";
 export default function Footer({ closeMenu }) {
 	return (
 		<footer className="footer">
-			<div className="footer-content">
-				<div className="footer-links">
-					<ul>
-						<li>
-							<Link to="/" onClick={closeMenu}>
-								Home
-							</Link>
-						</li>
-						<li>
-							<Link to="/about" onClick={closeMenu}>
-								About
-							</Link>
-						</li>
-						<li>
-							<Link to="/contact" onClick={closeMenu}>
-								Contact
-							</Link>
-						</li>
-						<li>
-							<Link to="/projects" onClick={closeMenu}>
-								Projects
-							</Link>
-						</li>
-						<li>
-							<Link to="/resume" onClick={closeMenu}>
-								Resume
-							</Link>
-						</li>
-						<li>
-							<Link to="/skills" onClick={closeMenu}>
-								Skills
-							</Link>
-						</li>
-					</ul>
-				</div>
-				<div className="footer-social">
-					<ul>
-						<li>
-							<a
-								href="https://linkedin.com"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img src="/footer/linkedin.png" alt="LinkedIn" />
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://facebook.com"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img src="/footer/facebook.png" alt="Facebook" />
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://www.instagram.com/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img src="/footer/instagram.png" alt="Instagram" />
-							</a>
-						</li>
-					</ul>
-				</div>
+			<div className="footer-links">
+				<ul>
+					<li>
+						<Link to="/" onClick={closeMenu}>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to="/about" onClick={closeMenu}>
+							About
+						</Link>
+					</li>
+					<li>
+						<Link to="/contact" onClick={closeMenu}>
+							Contact
+						</Link>
+					</li>
+					<li>
+						<Link to="/projects" onClick={closeMenu}>
+							Projects
+						</Link>
+					</li>
+					<li>
+						<Link to="/resume" onClick={closeMenu}>
+							Resume
+						</Link>
+					</li>
+					<li>
+						<Link to="/skills" onClick={closeMenu}>
+							Skills
+						</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="footer-social">
+				<ul>
+					<li>
+						<a
+							href="https://linkedin.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="/footer/linkedin.png" alt="LinkedIn" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://facebook.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="/footer/facebook.png" alt="Facebook" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.instagram.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="/footer/instagram.png" alt="Instagram" />
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="footer-bottom">
+				&copy; {new Date().getFullYear()} Ankur halder. All rights reserved.
 			</div>
 		</footer>
 	);
