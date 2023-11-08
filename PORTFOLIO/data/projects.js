151b6eac6e5175a2c3cf7c32f338336f770c7b73
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-09 02:43:18 +0530

    projects.js added

---
+++ PORTFOLIO/data/projects.js
@@ -0,0 +1,28 @@
+const projectsData = [
+	{
+		title: "AdComSys 2024",
+		date: "May 2023 - Sep 2023",
+		link: "https://adcomsys.uem.edu.in/",
+		description:
+			'Co-created Adcomsys conference website in collaboration with a classmate. Built using Next.js, React.js, and SCSS. Scheduled for 2024. For more information, please type "who created this website?" in the chatbot of the official website.',
+		image: "/projects/adcomsys.png",
+	},
+	{
+		title: "Space Otaku",
+		date: "Sep 2023 - Present",
+		link: "https://www.spaceotaku.online/",
+		description:
+			"Space Otaku: Explore space news, astronaut updates, satellite tracking, and more! Powered by Next.js, Sass, and Dialogflow for an immersive, interactive experience. Stay informed about the cosmos.",
+		image: "projects/spaceotaku.png",
+	},
+	{
+		title: "Universa",
+		date: "Jul 2023 - Present",
+		link: "https://universa2021.github.io/",
+		description:
+			"As the project leader, I spearheaded the development of Universa URP System, enhancing academic processes, and showcasing my exceptional leadership and project management skills, culminating in a successful college group project.",
+		image: "projects/universa.png",
+	},
+];
+
+export default projectsData;
