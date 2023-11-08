6252a19669b8b77ebe8dd3f3c81b297f0e3efe94
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-09 01:08:24 +0530

    updated scructure

--- PORTFOLIO/page/MySkills.jsx
+++ PORTFOLIO/page/MySkills.jsx
@@ -1,16 +1,10 @@
 function MySkills() {
 	const skills = [
 		{
-			name: "MongoDB",
+			name: "JavaScript",
 			level: "Expert",
 			description:
-				"MongoDB is a NoSQL database used in building robust and scalable backends. It allows me to work with data in a flexible and efficient way, making it ideal for handling large volumes of data. This skill can help recruiters by ensuring efficient data management and storage for web applications.",
-		},
-		{
-			name: "Express.js",
-			level: "Expert",
-			description:
-				"Express.js is a web application framework for Node.js. It simplifies the process of building powerful, feature-rich web applications and APIs, providing routing, middleware, and more. This skill can benefit recruiters by enabling the development of efficient and performant web services.",
+				"JavaScript is the backbone of web development. I use it to create interactive and responsive websites, providing a great user experience. This skill can help recruiters by ensuring the development of dynamic and engaging web applications.",
 		},
 		{
 			name: "React",
@@ -19,16 +13,34 @@ function MySkills() {
 				"React is a popular JavaScript library for building user interfaces. It enables me to create dynamic and interactive front-end applications, providing a seamless user experience. This skill can help recruiters by ensuring the creation of engaging and user-friendly web interfaces.",
 		},
 		{
-			name: "Node.js",
+			name: "Next.js",
 			level: "Expert",
+			description:
+				"Next.js is a React framework for building server-rendered applications. It provides enhanced performance and SEO capabilities for web apps. This skill can help recruiters by ensuring faster and SEO-friendly web applications.",
+		},
+		{
+			name: "Node.js",
+			level: "Intermediate",
 			description:
 				"Node.js is a server-side runtime environment. It allows me to develop scalable network applications, making it perfect for building backend services and APIs. This skill can benefit recruiters by ensuring the development of fast and efficient server-side applications.",
 		},
 		{
-			name: "JavaScript",
-			level: "Expert",
+			name: "Express.js",
+			level: "Intermediate",
 			description:
-				"JavaScript is the backbone of web development. I use it to create interactive and responsive websites, providing a great user experience. This skill can help recruiters by ensuring the development of dynamic and engaging web applications.",
+				"Express.js is a web application framework for Node.js. It simplifies the process of building powerful, feature-rich web applications and APIs, providing routing, middleware, and more. This skill can benefit recruiters by enabling the development of efficient and performant web services.",
+		},
+		{
+			name: "MongoDB",
+			level: "Intermediate",
+			description:
+				"MongoDB is a NoSQL database used in building robust and scalable backends. It allows me to work with data in a flexible and efficient way, making it ideal for handling large volumes of data. This skill can help recruiters by ensuring efficient data management and storage for web applications.",
+		},
+		{
+			name: "Mongoose",
+			level: "Intermediate",
+			description:
+				"Mongoose is an ODM library for MongoDB that simplifies the interaction with MongoDB databases. It provides a structured way to model data, validate inputs, and work with database queries. This skill can help recruiters by ensuring efficient and organized data modeling and interaction with MongoDB.",
 		},
 		{
 			name: "HTML",
@@ -49,10 +61,22 @@ function MySkills() {
 				"SCSS is a CSS preprocessor that simplifies styling. It helps me write more maintainable and organized stylesheets, saving time and effort. This skill can benefit recruiters by ensuring cleaner and more maintainable code for styling web applications.",
 		},
 		{
-			name: "Next.js",
+			name: "Git",
 			level: "Intermediate",
 			description:
-				"Next.js is a React framework for building server-rendered applications. It provides enhanced performance and SEO capabilities for web apps. This skill can help recruiters by ensuring faster and SEO-friendly web applications.",
+				"Git is a distributed version control system. It helps me collaborate on projects, track changes, and maintain code repositories efficiently. This skill can benefit recruiters by ensuring organized and version-controlled code management for projects.",
+		},
+		{
+			name: "GitHub",
+			level: "Intermediate",
+			description:
+				"GitHub is a platform for hosting and sharing code. It allows me to showcase my work, collaborate with others, and contribute to open-source projects. This skill can benefit recruiters by showcasing a strong online presence and collaboration abilities in the developer community.",
+		},
+		{
+			name: "Data Structures",
+			level: "Expert",
+			description:
+				"Understanding data structures is essential for efficient algorithm design and problem-solving. It enables me to optimize code and build efficient solutions. This skill can benefit recruiters by demonstrating problem-solving and algorithmic skills for efficient software development.",
 		},
 		{
 			name: "Python",
@@ -62,27 +86,9 @@ function MySkills() {
 		},
 		{
 			name: "C",
-			level: "Intermediate",
+			level: "Expert",
 			description:
 				"C is a fundamental programming language. It's used in system programming, embedded systems, and low-level development, giving me a deeper understanding of computer architecture. This skill can benefit recruiters by demonstrating a strong foundation in low-level programming and system development.",
-		},
-		{
-			name: "Git",
-			level: "Expert",
-			description:
-				"Git is a distributed version control system. It helps me collaborate on projects, track changes, and maintain code repositories efficiently. This skill can benefit recruiters by ensuring organized and version-controlled code management for projects.",
-		},
-		{
-			name: "GitHub",
-			level: "Expert",
-			description:
-				"GitHub is a platform for hosting and sharing code. It allows me to showcase my work, collaborate with others, and contribute to open-source projects. This skill can benefit recruiters by showcasing a strong online presence and collaboration abilities in the developer community.",
-		},
-		{
-			name: "Data Structures",
-			level: "Expert",
-			description:
-				"Understanding data structures is essential for efficient algorithm design and problem-solving. It enables me to optimize code and build efficient solutions. This skill can benefit recruiters by demonstrating problem-solving and algorithmic skills for efficient software development.",
 		},
 	];
 
