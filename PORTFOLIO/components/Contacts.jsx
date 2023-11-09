bac2fae1dd57220364b13ec84739e8f8ef9ea033
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-09 20:17:14 +0530

    contact component added

---
+++ PORTFOLIO/components/Contacts.jsx
@@ -0,0 +1,44 @@
+function Contacts() {
+	return (
+		<div>
+			<h2>Contact Information</h2>
+			<p>Email: ankur.halder12345@gmail.com</p>
+			<p>Phone: +91 97489 03490</p>
+			<p>Nationality: Indian</p>
+			<div className="sotials">
+				<span>Connect Me With:</span>
+				<ul>
+					<li>
+						<a
+							href="https://www.linkedin.com/in/ankur-halder-a19553188/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="/footer/linkedin.png" alt="LinkedIn" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.facebook.com/ankur.roxx.9"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="/footer/facebook.png" alt="Facebook" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.instagram.com/halder_ankur/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src="/footer/instagram.png" alt="Instagram" />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Contacts;
