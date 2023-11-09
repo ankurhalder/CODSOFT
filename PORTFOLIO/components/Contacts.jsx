ae72f8a09dbe0b01614b729024226f5c52a95775
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-11-09 20:48:27 +0530

    v25

--- PORTFOLIO/components/Contacts.jsx
+++ PORTFOLIO/components/Contacts.jsx
@@ -1,59 +1,62 @@
 function Contacts() {
 	return (
-		<div className="contacts-container">
-			<h2 className="contacts-heading">Contact Information</h2>
-			<p className="contacts-email contacts-detail">
-				<span className="detail-label">Email:</span> ankur.halder12345@gmail.com
-			</p>
-			<p className="contacts-phone contacts-detail">
-				<span className="detail-label">Phone:</span> +91 97489 03490
-			</p>
-			<p className="contacts-nationality contacts-detail">
-				<span className="detail-label">Nationality:</span> Indian
-			</p>
-			<div className="social-links-container">
-				<span className="connect-label">Connect Me With:</span>
-				<ul className="social-links-list">
-					<li className="social-link-item">
-						<a
-							href="https://www.linkedin.com/in/ankur-halder-a19553188/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<img
-								src="/footer/linkedin.png"
-								alt="LinkedIn"
-								className="social-icon"
-							/>
-						</a>
-					</li>
-					<li className="social-link-item">
-						<a
-							href="https://www.facebook.com/ankur.roxx.9"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<img
-								src="/footer/facebook.png"
-								alt="Facebook"
-								className="social-icon"
-							/>
-						</a>
-					</li>
-					<li className="social-link-item">
-						<a
-							href="https://www.instagram.com/halder_ankur/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<img
-								src="/footer/instagram.png"
-								alt="Instagram"
-								className="social-icon"
-							/>
-						</a>
-					</li>
-				</ul>
+		<div className="contact-container">
+			<div className="contacts-container">
+				<h2 className="contacts-heading">Contact Information</h2>
+				<p className="contacts-email contacts-detail">
+					<span className="detail-label">Email:</span>{" "}
+					ankur.halder12345@gmail.com
+				</p>
+				<p className="contacts-phone contacts-detail">
+					<span className="detail-label">Phone:</span> +91 97489 03490
+				</p>
+				<p className="contacts-nationality contacts-detail">
+					<span className="detail-label">Nationality:</span> Indian
+				</p>
+				<div className="social-links-container">
+					<span className="connect-label">Connect Me With:</span>
+					<ul className="social-links-list">
+						<li className="social-link-item">
+							<a
+								href="https://www.linkedin.com/in/ankur-halder-a19553188/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img
+									src="/footer/linkedin.png"
+									alt="LinkedIn"
+									className="social-icon"
+								/>
+							</a>
+						</li>
+						<li className="social-link-item">
+							<a
+								href="https://www.facebook.com/ankur.roxx.9"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img
+									src="/footer/facebook.png"
+									alt="Facebook"
+									className="social-icon"
+								/>
+							</a>
+						</li>
+						<li className="social-link-item">
+							<a
+								href="https://www.instagram.com/halder_ankur/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img
+									src="/footer/instagram.png"
+									alt="Instagram"
+									className="social-icon"
+								/>
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
