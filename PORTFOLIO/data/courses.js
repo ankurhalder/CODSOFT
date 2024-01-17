3395ec967e1215c21e7f6f2658ca7bd604fd158d
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-01-18 03:30:40 +0530

    all data has been updated

--- PORTFOLIO/data/courses.js
+++ PORTFOLIO/data/courses.js
@@ -1,60 +1,67 @@
 const courseData = [
-	{
-		title: "Next.js & React - The Complete Guide (incl. Two Paths!)",
-		institution: "udemy, Online",
-		duration: "Jul 2023 - Sep 2023",
-		certificateLink:
-			"https://www.udemy.com/certificate/UC-fc0143a4-6b19-4e07-bcee-53f7c1f9fd1d/",
-	},
-	{
-		title: "The Ultimate React Course 2023: React, Redux & More",
-		institution: "udemy, Online",
-		duration: "Mar 2023 - Jul 2023",
-		certificateLink:
-			"https://www.udemy.com/certificate/UC-3b120d82-005f-43b9-a60e-2fe949bb1f8c/",
-	},
-	{
-		title: "Meta Front-End Developer (Professional Certificate)",
-		institution: "Coursera, Online",
-		duration: "Feb 2023 - Aug 2023",
-		certificateLink:
-			"https://www.coursera.org/account/accomplishments/specialization/certificate/4GVM3WFXRHB6/",
-		credentialID: "4GVM3WFXRHB6",
-		skills: [
-			"🚀 Introduction to Front-End Development",
-			"🚀 HTML and CSS in Depth",
-			"🚀 Programming with JavaScript",
-			"🚀 React Basics",
-			"🚀 Advanced React",
-			"🚀 Coding Interview Preparation",
-			"🚀 Version Control",
-			"🚀 Principles of UX/UI Design",
-			"🚀 Front-End Developer Capstone",
-		],
-	},
-	{
-		title: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
-		institution: "udemy, Online",
-		duration: "Jan 2023 - Sep 2023",
-		certificateLink:
-			"https://www.udemy.com/certificate/UC-ea6816f6-5c07-4069-870d-65486f34d3ef/",
-	},
-	{
-		title: "Explore A Career In Front-End Web Development",
-		institution: "LinkedIn, Online",
-		duration: "Dec 2022 - Aug 2023",
-		certificateLink:
-			"https://www.linkedin.com/learning/certificates/26a8bcdc2bbda4c723aa1c3b18d6bdfdc163050f32c3a7f62d7fef380be07d4d?u=229219690",
-		description:
-			"I have completed a comprehensive learning path in front-end web development, complemented by a certification. This path has equipped me with the essential skills to design and develop engaging, interactive user experiences on the web, including proficiency in HTML, CSS, JavaScript, and React. Furthermore, I have gained knowledge in accessibility techniques and a deep understanding of web concepts, enabling me to create inclusive and user-friendly web applications.",
-	},
-	{
-		title: "The Complete JavaScript Course 2023: From Zero To Expert!",
-		institution: "udemy, Online",
-		duration: "Aug 2022 - Jan 2023",
-		certificateLink:
-			"https://www.udemy.com/certificate/UC-19c4ac79-892a-47ad-b473-06600f7300c8/",
-	},
+  {
+    title: "Programming in Python",
+    institution: "coursera, Online",
+    duration: "dec 2023- jan 2024",
+    certificateLink:
+      "https://www.coursera.org/account/accomplishments/verify/9P54MC7FPBMC/",
+  },
+  {
+    title: "Next.js & React - The Complete Guide (incl. Two Paths!)",
+    institution: "udemy, Online",
+    duration: "Jul 2023 - Sep 2023",
+    certificateLink:
+      "https://www.udemy.com/certificate/UC-fc0143a4-6b19-4e07-bcee-53f7c1f9fd1d/",
+  },
+  {
+    title: "The Ultimate React Course 2023: React, Redux & More",
+    institution: "udemy, Online",
+    duration: "Mar 2023 - Jul 2023",
+    certificateLink:
+      "https://www.udemy.com/certificate/UC-3b120d82-005f-43b9-a60e-2fe949bb1f8c/",
+  },
+  {
+    title: "Meta Front-End Developer (Professional Certificate)",
+    institution: "Coursera, Online",
+    duration: "Feb 2023 - Aug 2023",
+    certificateLink:
+      "https://www.coursera.org/account/accomplishments/specialization/certificate/4GVM3WFXRHB6/",
+    credentialID: "4GVM3WFXRHB6",
+    skills: [
+      "🚀 Introduction to Front-End Development",
+      "🚀 HTML and CSS in Depth",
+      "🚀 Programming with JavaScript",
+      "🚀 React Basics",
+      "🚀 Advanced React",
+      "🚀 Coding Interview Preparation",
+      "🚀 Version Control",
+      "🚀 Principles of UX/UI Design",
+      "🚀 Front-End Developer Capstone",
+    ],
+  },
+  {
+    title: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
+    institution: "udemy, Online",
+    duration: "Jan 2023 - Sep 2023",
+    certificateLink:
+      "https://www.udemy.com/certificate/UC-ea6816f6-5c07-4069-870d-65486f34d3ef/",
+  },
+  {
+    title: "Explore A Career In Front-End Web Development",
+    institution: "LinkedIn, Online",
+    duration: "Dec 2022 - Aug 2023",
+    certificateLink:
+      "https://www.linkedin.com/learning/certificates/26a8bcdc2bbda4c723aa1c3b18d6bdfdc163050f32c3a7f62d7fef380be07d4d?u=229219690",
+    description:
+      "I have completed a comprehensive learning path in front-end web development, complemented by a certification. This path has equipped me with the essential skills to design and develop engaging, interactive user experiences on the web, including proficiency in HTML, CSS, JavaScript, and React. Furthermore, I have gained knowledge in accessibility techniques and a deep understanding of web concepts, enabling me to create inclusive and user-friendly web applications.",
+  },
+  {
+    title: "The Complete JavaScript Course 2023: From Zero To Expert!",
+    institution: "udemy, Online",
+    duration: "Aug 2022 - Jan 2023",
+    certificateLink:
+      "https://www.udemy.com/certificate/UC-19c4ac79-892a-47ad-b473-06600f7300c8/",
+  },
 ];
 
 export default courseData;
--- PORTFOLIO/data/skills.js
+++ PORTFOLIO/data/skills.js
@@ -1,93 +1,105 @@
 const skills = [
-	{
-		name: "JavaScript",
-		level: "Expert",
-		description:
-			"JavaScript is the backbone of web development. I use it to create interactive and responsive websites, providing a great user experience. This skill can help recruiters by ensuring the development of dynamic and engaging web applications.",
-	},
-	{
-		name: "React",
-		level: "Expert",
-		description:
-			"React is a popular JavaScript library for building user interfaces. It enables me to create dynamic and interactive front-end applications, providing a seamless user experience. This skill can help recruiters by ensuring the creation of engaging and user-friendly web interfaces.",
-	},
-	{
-		name: "Next.js",
-		level: "Expert",
-		description:
-			"Next.js is a React framework for building server-rendered applications. It provides enhanced performance and SEO capabilities for web apps. This skill can help recruiters by ensuring faster and SEO-friendly web applications.",
-	},
-	{
-		name: "Node.js",
-		level: "Intermediate",
-		description:
-			"Node.js is a server-side runtime environment. It allows me to develop scalable network applications, making it perfect for building backend services and APIs. This skill can benefit recruiters by ensuring the development of fast and efficient server-side applications.",
-	},
-	{
-		name: "Express.js",
-		level: "Intermediate",
-		description:
-			"Express.js is a web application framework for Node.js. It simplifies the process of building powerful, feature-rich web applications and APIs, providing routing, middleware, and more. This skill can benefit recruiters by enabling the development of efficient and performant web services.",
-	},
-	{
-		name: "MongoDB",
-		level: "Intermediate",
-		description:
-			"MongoDB is a NoSQL database used in building robust and scalable backends. It allows me to work with data in a flexible and efficient way, making it ideal for handling large volumes of data. This skill can help recruiters by ensuring efficient data management and storage for web applications.",
-	},
-	{
-		name: "Mongoose",
-		level: "Intermediate",
-		description:
-			"Mongoose is an ODM library for MongoDB that simplifies the interaction with MongoDB databases. It provides a structured way to model data, validate inputs, and work with database queries. This skill can help recruiters by ensuring efficient and organized data modeling and interaction with MongoDB.",
-	},
-	{
-		name: "HTML",
-		level: "Expert",
-		description:
-			"HTML is the foundation of web pages. It helps structure the content and layout of a website, making it accessible and user-friendly. This skill can benefit recruiters by ensuring the proper structuring of web content for better user experiences.",
-	},
-	{
-		name: "CSS",
-		level: "Expert",
-		description:
-			"CSS is crucial for styling web pages. It allows me to design and enhance the visual aspects of websites, making them visually appealing. This skill can help recruiters by ensuring attractive and visually pleasing web designs.",
-	},
-	{
-		name: "SCSS",
-		level: "Intermediate",
-		description:
-			"SCSS is a CSS preprocessor that simplifies styling. It helps me write more maintainable and organized stylesheets, saving time and effort. This skill can benefit recruiters by ensuring cleaner and more maintainable code for styling web applications.",
-	},
-	{
-		name: "Git",
-		level: "Intermediate",
-		description:
-			"Git is a distributed version control system. It helps me collaborate on projects, track changes, and maintain code repositories efficiently. This skill can benefit recruiters by ensuring organized and version-controlled code management for projects.",
-	},
-	{
-		name: "GitHub",
-		level: "Intermediate",
-		description:
-			"GitHub is a platform for hosting and sharing code. It allows me to showcase my work, collaborate with others, and contribute to open-source projects. This skill can benefit recruiters by showcasing a strong online presence and collaboration abilities in the developer community.",
-	},
-	{
-		name: "Data Structures",
-		level: "Expert",
-		description:
-			"Understanding data structures is essential for efficient algorithm design and problem-solving. It enables me to optimize code and build efficient solutions. This skill can benefit recruiters by demonstrating problem-solving and algorithmic skills for efficient software development.",
-	},
-	{
-		name: "Python",
-		level: "Intermediate",
-		description:
-			"Python is a versatile and powerful programming language. I use it for various tasks, including web development, data analysis, and automation. This skill can benefit recruiters by offering a wide range of application possibilities, from web development to data processing.",
-	},
-	{
-		name: "C",
-		level: "Expert",
-		description:
-			"C is a fundamental programming language. It's used in system programming, embedded systems, and low-level development, giving me a deeper understanding of computer architecture. This skill can benefit recruiters by demonstrating a strong foundation in low-level programming and system development.",
-	},
+  {
+    name: "JavaScript",
+    level: "Expert",
+    description:
+      "JavaScript is the backbone of web development. I use it to create interactive and responsive websites, providing a great user experience. This skill can help recruiters by ensuring the development of dynamic and engaging web applications.",
+  },
+  {
+    name: "React",
+    level: "Expert",
+    description:
+      "React is a popular JavaScript library for building user interfaces. It enables me to create dynamic and interactive front-end applications, providing a seamless user experience. This skill can help recruiters by ensuring the creation of engaging and user-friendly web interfaces.",
+  },
+  {
+    name: "Next.js",
+    level: "Expert",
+    description:
+      "Next.js is a React framework for building server-rendered applications. It provides enhanced performance and SEO capabilities for web apps. This skill can help recruiters by ensuring faster and SEO-friendly web applications.",
+  },
+  {
+    name: "Node.js",
+    level: "Intermediate",
+    description:
+      "Node.js is a server-side runtime environment. It allows me to develop scalable network applications, making it perfect for building backend services and APIs. This skill can benefit recruiters by ensuring the development of fast and efficient server-side applications.",
+  },
+  {
+    name: "Express.js",
+    level: "Intermediate",
+    description:
+      "Express.js is a web application framework for Node.js. It simplifies the process of building powerful, feature-rich web applications and APIs, providing routing, middleware, and more. This skill can benefit recruiters by enabling the development of efficient and performant web services.",
+  },
+  {
+    name: "MongoDB",
+    level: "Intermediate",
+    description:
+      "MongoDB is a NoSQL database used in building robust and scalable backends. It allows me to work with data in a flexible and efficient way, making it ideal for handling large volumes of data. This skill can help recruiters by ensuring efficient data management and storage for web applications.",
+  },
+  {
+    name: "Mongoose",
+    level: "Intermediate",
+    description:
+      "Mongoose is an ODM library for MongoDB that simplifies the interaction with MongoDB databases. It provides a structured way to model data, validate inputs, and work with database queries. This skill can help recruiters by ensuring efficient and organized data modeling and interaction with MongoDB.",
+  },
+  {
+    name: "HTML",
+    level: "Expert",
+    description:
+      "HTML is the foundation of web pages. It helps structure the content and layout of a website, making it accessible and user-friendly. This skill can benefit recruiters by ensuring the proper structuring of web content for better user experiences.",
+  },
+  {
+    name: "CSS",
+    level: "Expert",
+    description:
+      "CSS is crucial for styling web pages. It allows me to design and enhance the visual aspects of websites, making them visually appealing. This skill can help recruiters by ensuring attractive and visually pleasing web designs.",
+  },
+  {
+    name: "SCSS",
+    level: "Intermediate",
+    description:
+      "SCSS is a CSS preprocessor that simplifies styling. It helps me write more maintainable and organized stylesheets, saving time and effort. This skill can benefit recruiters by ensuring cleaner and more maintainable code for styling web applications.",
+  },
+  {
+    name: "Git",
+    level: "Intermediate",
+    description:
+      "Git is a distributed version control system. It helps me collaborate on projects, track changes, and maintain code repositories efficiently. This skill can benefit recruiters by ensuring organized and version-controlled code management for projects.",
+  },
+  {
+    name: "GitHub",
+    level: "Intermediate",
+    description:
+      "GitHub is a platform for hosting and sharing code. It allows me to showcase my work, collaborate with others, and contribute to open-source projects. This skill can benefit recruiters by showcasing a strong online presence and collaboration abilities in the developer community.",
+  },
+  {
+    name: "Data Structures",
+    level: "Expert",
+    description:
+      "Understanding data structures is essential for efficient algorithm design and problem-solving. It enables me to optimize code and build efficient solutions. This skill can benefit recruiters by demonstrating problem-solving and algorithmic skills for efficient software development.",
+  },
+  {
+    name: "Python",
+    level: "Intermediate",
+    description:
+      "Python is a versatile and powerful programming language. I use it for various tasks, including web development, data analysis, and automation. This skill can benefit recruiters by offering a wide range of application possibilities, from web development to data processing.",
+  },
+  {
+    name: "C",
+    level: "Expert",
+    description:
+      "C is a fundamental programming language. It's used in system programming, embedded systems, and low-level development, giving me a deeper understanding of computer architecture. This skill can benefit recruiters by demonstrating a strong foundation in low-level programming and system development.",
+  },
+  {
+    name: "SQL",
+    level: "Intermediate",
+    description:
+      "SQL (Structured Query Language) is a domain-specific language used for managing and manipulating relational databases. I have experience in writing SQL queries to retrieve, update, and manage database data. This skill is essential for efficient and organized data management in applications.",
+  },
+  {
+    name: "MySQL",
+    level: "Intermediate",
+    description:
+      "MySQL is a widely used open-source relational database management system. I am proficient in designing and interacting with MySQL databases, ensuring data integrity and optimal performance. This skill can benefit recruiters by ensuring reliable and scalable data storage solutions.",
+  },
 ];
 export default skills;
